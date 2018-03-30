0f2fd41ef3e2b63be96c55823871e324422f4cd5
Collin <collinsconway@gmail.com>
2018-03-29 17:33:39 -0700

    added setup scripts

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+printf "\nCreating virtual environment\n\n"
+pip3 install virtualenv --user
+python3 -m venv ./.virtualenv
+
+printf "\nRunning virtual environment\n\n"
+chmod u+x ./.virtualenv/bin/activate
+source ./.virtualenv/bin/activate py35
+
+
+printf "Installing prerequisites\n\n"
+pip3 install -r requirements.txt
+
+cd application
+printf "\nRunning application\n\n"
+python3 ./hello_world.py
+
+
+deactivate
+
