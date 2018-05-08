19cbcda5792ebd957b109af994b0c3d3b3601b2f
Adam Krpan <adam.krpan@outlook.com>
2018-05-08 11:54:25 -0700

    Added duration normal graph and labels to genre graphs

--- application/visualization/duration_breakdown.py
+++ application/visualization/duration_breakdown.py
@@ -9,6 +9,7 @@ import matplotlib as mpl
 mpl.use('Agg')  # Necessary so we can work without Tkinter
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
 
 
 def generate_hist(data: list, binsize: int, filename: str):
@@ -20,6 +21,16 @@ def generate_hist(data: list, binsize: int, filename: str):
     plt.ylabel("Count", fontsize=14)
     plt.xticks([x + (binsize / 2) for x in range(0, 800 - binsize, binsize)], rotation=75)
     plt.yticks(fontsize=10)
+    plt.savefig(filename, dpi=480)
+
+
+def generate_norm(data: list, filename: str):
+    plt.close()
+    plt.figure(figsize=(11, 7))
+    plt.title("Song Duration", fontsize=18)
+    fit = norm.pdf(sorted(data), np.mean(data), np.std(data))
+    plt.plot(sorted(data), fit, '-')
+    plt.plot(data, [0] * len(data), 'o')
     plt.savefig(filename, dpi=480)
 
 
@@ -39,6 +50,8 @@ def main():
 
     for i in range(20, 51, 10):
         generate_hist(durations, i, 'visualizations/duration_hist_binsize_{}.png'.format(i))
+
+    generate_norm(durations, 'visualizations/duration_norm.png')
 
 
 if __name__ == '__main__':
